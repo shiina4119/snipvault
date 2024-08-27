ee9c875ed412316e9956a0f4ed9439d30eca9196
shiina <152995083+shiina4119@users.noreply.github.com>
2024-08-27 23:50:00 +0530

    Index page mockup

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,22 @@
-import ThemeToggle from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
 
 export default function Home() {
   return (
     <>
-      <ThemeToggle />
-      <main>Hello</main>
+      <div className="justify-between space-y-4 p-4 sm:flex sm:flex-row">
+        <Button>Language</Button>
+        <div className="flex items-center space-x-2">
+          <Label htmlFor="syntax">Syntax Highlighting</Label>
+          <Switch id="syntax" />
+        </div>
+      </div>
+      <div>Code Area</div>
+      <div className="flex flex-row justify-end space-x-4 p-4">
+        <Button>Expire after</Button>
+        <Button>Paste snippet</Button>
+      </div>
     </>
   );
 }
